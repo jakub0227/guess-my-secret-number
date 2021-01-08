0fbae229f6136b2d424d8be3a9c4fb35242d1a1e
jakub0227 <68559474+jakub0227@users.noreply.github.com>
2021-01-08 12:50:46 +0100

    Add GameOver screen, improve logic

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {Route} from '../../types/Route'
-import {Button, Card, Grid, Icon, Input, lighten, Slider, Typography} from '@material-ui/core'
+import {Box, Button, Card, Grid, Icon, Input, lighten, Slider, Typography} from '@material-ui/core'
 import {useSnackbar} from 'notistack'
 import useTheme from '@material-ui/core/styles/useTheme'
 import {css} from '@emotion/react'
@@ -31,7 +31,7 @@ export const GamePage: Route = () => {
 	
 	//LOGIC
 	const [value, setValue] = useState<number | string | Array<number | string>>(0)
-	const [attemptNr, setAttemptNr] = useState<number>(0)
+	const [attempts, setAttempts] = useState<number>(0)
 	const [gameOver, setGameOver] = useState(false)
 	const {enqueueSnackbar} = useSnackbar()
 	
@@ -49,89 +49,94 @@ export const GamePage: Route = () => {
 		}
 	}
 	
-	const EASY = 15
+	const EASY_MODE = 15
 	// const MEDIUM = 10
 	// const HARD = 5
 	
-	let attempts = 0
-	const endGame = EASY
+	const endGame = EASY_MODE
 	
 	const handleGameFunction = () => {
 		if (value === guessTheNumber) {
 			enqueueSnackbar('What a successful guess!', {
 				variant: 'success',
 			})
+			setGameOver(true)
 		}
 		if (value > guessTheNumber) {
 			enqueueSnackbar('Too high, guess again!', {
 				variant: 'error',
 			})
+			setAttempts(attempts + 1)
 		}
-		if (value < guessTheNumber) enqueueSnackbar('Too Low, guess again!', {
-			variant: 'error',
-		})
+		if (value < guessTheNumber) {
+			enqueueSnackbar('Too Low, guess again!', {
+				variant: 'error',
+			})
+			setGameOver(false)
+			setAttempts(attempts + 1)
+		}
 	}
 	
 	const handleClick = (e: React.ChangeEvent<{}>) => {
 		handleGameFunction()
 		e.preventDefault()
 	}
-
-// const onClickHandler = () => {
-//
-// 	let userGuess = value
-// 	if (userGuess === 0) return null
-// 	if (userGuess === targetNumber) enqueueSnackbar('What a successful guess!', {
-// 		variant: 'success',
-// 	})
-// 	if (userGuess > targetNumber) enqueueSnackbar('Too high, guess again!', {
-// 		variant: 'error',
-// 	})
-// 	if (userGuess < targetNumber) enqueueSnackbar('Too Low, guess again!', {
-// 		variant: 'error',
-// 	})
-// }
 	
 	return (
 		<div>
-			<Card elevation={7} css={styles.root}>
-				<Typography variant='h5'>
-					Try to guess the number...
-				</Typography>
-				<Grid css={styles.gameControl} container spacing={2} alignItems='center'>
-					<Grid item>
-						<Icon>emoji_emotions</Icon>
+			{!gameOver ? (
+				<Card elevation={7} css={styles.root}>
+					<Typography variant='h5'>
+						Try to guess the number...
+					</Typography>
+					<Grid css={styles.gameControl} container spacing={2} alignItems='center'>
+						<Grid item>
+							<Icon>emoji_emotions</Icon>
+						</Grid>
+						<Grid item xs>
+							<Slider
+								value={typeof value === 'number' ? value : 0}
+								onChange={handleSliderChange}
+							/>
+						</Grid>
+						<Grid item>
+							<Input
+								value={value}
+								margin='dense'
+								onChange={handleInputChange}
+								onBlur={handleBlur}
+								inputProps={{
+									step: 1,
+									min: 0,
+									max: 100,
+									type: 'number',
+								}}
+							/>
+						</Grid>
 					</Grid>
-					<Grid item xs>
-						<Slider
-							value={typeof value === 'number' ? value : 0}
-							onChange={handleSliderChange}
-						/>
-					</Grid>
-					<Grid item>
-						<Input
-							value={value}
-							margin='dense'
-							onChange={handleInputChange}
-							onBlur={handleBlur}
-							inputProps={{
-								step: 1,
-								min: 0,
-								max: 100,
-								type: 'number',
-							}}
-						/>
-					</Grid>
-				</Grid>
-				<Button
-					onClick={handleClick}
-					color='primary'
-					size='medium'
-					variant='contained'>
-					Guess
-				</Button>
-			</Card>
+					<Button
+						onClick={handleClick}
+						color='primary'
+						size='medium'
+						variant='contained'>
+						Guess
+					</Button>
+					<Typography variant='h6'>
+						Try nr: {attempts}
+					</Typography>
+				</Card>
+			) : (
+				<Box>
+					<Typography variant='h6'>
+						Congratulations!
+					</Typography>
+					<Typography variant='h6'>
+						You did it in {attempts} attempts.
+					</Typography>
+				</Box>
+			)}
 		</div>
+	
 	)
 }
 
